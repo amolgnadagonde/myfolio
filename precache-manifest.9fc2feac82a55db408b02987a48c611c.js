b6086126e9a5a24c1cedb0dd9de153cb071650f7
amolgnadagonde <amol.nadagonde@outlook.com>
2019-09-21 14:30:20 +0530

    Updates

---
+++ precache-manifest.9fc2feac82a55db408b02987a48c611c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "411855b696158b56c1c66a7eb384dd9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b0d6996893868dca5bb",
+    "url": "/static/css/main.22cd231b.chunk.css"
+  },
+  {
+    "revision": "16244bd518a375a44c19",
+    "url": "/static/js/2.156421fa.chunk.js"
+  },
+  {
+    "revision": "3b0d6996893868dca5bb",
+    "url": "/static/js/main.83cb5456.chunk.js"
+  },
+  {
+    "revision": "d7152a9b8461c7329e94",
+    "url": "/static/js/runtime~main.6c0f5cb8.js"
+  },
+  {
+    "revision": "4e35aef8ee3526512350ce05af7aad46",
+    "url": "/static/media/small-img.4e35aef8.jpg"
+  }
+]);
